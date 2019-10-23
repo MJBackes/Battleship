cdfa92e9d0d6601b09c3f9d81063c880a0adaa2c
MJBackes <mjohnbackes@gmail.com>
2019-10-23 15:37:22 -0500

    Added the classes tht will form the game's skeleton.

---
+++ Battleship/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Board
+    {
+        //MembVars
+        BoardSquare[][] Matrix;
+
+        //Constr
+        public Board()
+        {
+            Matrix = new BoardSquare[20][];
+            for(int i = 0; i < Matrix.Length; i++)
+            {
+                Matrix[i] = new BoardSquare[20];
+            }
+        }
+        //MembMeth
+        private void FillBoard()
+        {
+            for(int i = 0; i < Matrix.Length; i++)
+            {
+                for(int j = 0; j < Matrix[i].Length; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Battleship/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship
+{
+    class Player
+    {
+        //MembVars
+
+
+        //Constr
+
+        //MembMeth
+    }
+}
